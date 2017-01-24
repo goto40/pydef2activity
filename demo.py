62981ed6c172bb5947ac7f8757f24a1443491125
goto40 <pierre.bayerl@alumni.uni-ulm.de>
2017-01-24 22:35:28 +0100

    first code

---
+++ demo.py
@@ -0,0 +1,21 @@
+
+def bar():
+    print ("BAR")
+
+def foo(y=2):
+    x=3
+    #comment1
+    #activity TEST1
+    if x>2:
+        x=1
+        print(y)
+    #end activitiy
+    print(x*2)
+    if(x>y):
+        print(x*y)
+    elif(x==y):
+        y=y-1
+        print(x+y)        
+    else:
+        print(y)
+    return x+y
